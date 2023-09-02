e7400cde802a2916e69c5c00f3c59f447c8b77e4
mattraydev <cedaring.xr@gmail.com>
2023-09-02 13:37:45 -0600

    build test, removed some bits

--- lib/getUser.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default async function getUser(userId: string) {
-	const res = await fetch(`https://jsonplaceholder.typicode.com/user/${userId}`)
-
-	if (!res.ok) throw Error('failed to fetch user')
-	return res.json()
-}
--- lib/getUsers.ts
+++
@@ -1,7 +0,0 @@
-export default async function getUsers() {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users')
-
-	if (!res.ok) throw Error('failed to fetch data')
-
-	return res.json()
-}
